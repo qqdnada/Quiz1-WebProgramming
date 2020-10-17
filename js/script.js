2b8959e735da034f546142e1f78d5dc391ecf746
Qatrunada Qori Darwati <57135989+qqdnada@users.noreply.github.com>
2020-10-17 17:42:33 +0800

    Add css and javascript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var button1 = document.querySelector("button")
+
+button1.addEventListener('click', function(){
+    alert("Sorry, this feature is under development.");
+})
+
+var button2 = document.querySelector(".secondary")
+
+button2.addEventListener('click', function(){
+    alert("Sorry, this feature is under development.");
+})
